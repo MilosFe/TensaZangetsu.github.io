db98b47d2f5c389114333eb5246a8956570b89b0
TensaZangetsu <l.lawliet46@gmail.com>
2016-03-18 12:40:01 +0100

    Bit of refactoring

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -3,29 +3,22 @@
             "Designer",
             "Coder"];
     var i = 0;
-
+//Closure should not be after break up
     function type() {
         $("#container").html("");
         $.each(x[i].split(''), function (index, letter) {
-
-            //we add 100*i ms delay to each letter 
+            //we add 200*i ms delay to each letter 
             setTimeout(function () {
-
                 //we add the letter to the container
                 $('#container').html($('#container').html() + letter);
 
-            }, 100 * index);
+            }, 200 * index);
         });
-
         i++;
         if (i > 2) {
             i = 0;
         }
     }
-
-
-    window.setInterval(type, 3000);
-
-
+    window.setInterval(type, 6000);
 
 })(this, jQuery)
